c9d8caf0883ac5a85d8d6039356589a65245a5a5
Sttrrr <mekad12@mail.ru>
2023-04-22 13:57:44 +0300

    разделение

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:docum/docScreen.dart';
 import 'package:docum/resources/resources.dart';
+import 'package:docum/widgets/big_text_widget.dart';
 import 'package:docum/widgets/media_stack.dart';
+import 'package:docum/widgets/small_text_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -25,53 +27,101 @@ const HomePage({ Key? key }) : super(key: key);
         ),
       ),),
       body: Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text("Главная",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.grey[500]
-                ),
-                ),
-                Icon(
-                  Icons.more_horiz,
-                  size: 30,
-                  color: Colors.grey[500],
-                )
-              ],
-            ),
-            SizedBox(height: 20,),
-            
-            Container(
-              height: 250,
-              child:ListView(
-                physics: BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-               MediaStack(image: Images.document, color: Color.fromARGB(106, 76, 175, 79), media: 'Документы', items: "0", privacy: "Private Folder",),
-                  SizedBox(width: 15),
-                   MediaStack(image: Images.person, color: Color.fromARGB(148, 255, 193, 79), media: 'Сотрудники ', items: "0", privacy: "Private Folder",  ),
-                   SizedBox(width: 15),
-                    MediaStack(image: Images.request, color: Color.fromARGB(104, 223, 64, 79), media: 'Поданые заявки', items: "0", privacy: "Private Folder",)
+                  Text(
+                    "Главная",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.grey[500]
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    size: 30,
+                    color: Colors.grey[500],
+                  )
                 ],
               ),
-            ),
-            SizedBox(height: 25,),
-            Text("Ваши активные тендеры:",
-            style: TextStyle(
-              fontSize: 25,
-            ),),
+              SizedBox(height: 20,),
 
-          ],
+              Container(
+                height: 250,
+                child:ListView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    MediaStack(image: Images.document, color: Color.fromARGB(106, 76, 175, 79), media: 'Документы', items: "0", privacy: "Private Folder",),
+                    SizedBox(width: 15),
+                    MediaStack(image: Images.person, color: Color.fromARGB(148, 255, 193, 79), media: 'Сотрудники ', items: "0", privacy: "Private Folder", ),
+                    SizedBox(width: 15),
+                    MediaStack(image: Images.request, color: Color.fromARGB(104, 223, 64, 79), media: 'Заявки', items: "0", privacy: "Private Folder",)
+                  ],
+                ),
+              ),
+              SizedBox(height: 25,),
+              Text("Активные тендеры :",
+                style: TextStyle(
+                  fontSize: 25,
+
+                ),),
+              Container(
+                margin: EdgeInsets.only(
+                    right: 20,
+                    left: 20,
+                    bottom: 20),
+                child: Row(
+                    children: [
+                      //текст
+                      Expanded(
+                        child: Container(
+                          height: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topRight:
+                              Radius.circular(15),
+                              bottomRight:
+                              Radius.circular(15),
+                            ),
+                            color: Colors.white,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 10,
+                                right: 15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceAround,
+                              children: [
+                                BigText(
+                                    text: "Установка и замена барьерного ограждения"),
+                                SmallText(
+                                    text:
+                                    "срок: 2 месяца"),
+                                SmallText(
+                                    text:
+                                    "Условия: Рабочая сила"),
+                                SmallText(
+                                    text:
+                                    "Цена: до 4935 руб/м.п"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home:  HomeUserPage(),
+      home:  HiddenDrawer(),
     );
   }
 }
